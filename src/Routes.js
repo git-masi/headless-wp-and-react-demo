ac823ca424fdf2cf0355c4c90d45eb10e27b7ea6
Eric Masi <eric@gitmasi.com>
2019-06-05 17:00:02 -0400

    add Cities

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
-  
+import Cities from './components/Cities';
+
 class Routes extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" render={() => <Home />}/>
-        <Route exact path="/cities" render={() => <h1>cities page</h1>}/>
+        <Route exact path="/cities" render={() => <Cities />}/>
         <Route exact path="/bikes" render={() => <h1>bikes page</h1>}/>
       </Switch>
     )
---
+++ src/components/Cities.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+import Navbar from './Navbar';
+
+class Cities extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Navbar />
+      </Fragment>
+    )
+  }
+}
+
+export default Cities;
