4f7fc7a11e789f47cf67c30663de3878366e2509
Eric Masi <eric@gitmasi.com>
2019-06-05 16:43:13 -0400

    add WP API request,  render response in HomeCard

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,34 @@
 import React, { Component, Fragment } from 'react';
+import axios from 'axios';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import HomeCard from './HomeCard';
 import styles from './Home.module.css';
   
 class Home extends Component {
+  state = {
+    cities: [],
+    isLoaded: false,
+  }
+
+  componentDidMount() {
+    axios.get('/wp-json/wp/v2/cities')
+      .then(res => this.setState({
+        cities: res.data,
+        isLoaded: true,
+      }))
+      .catch(err => console.log(err));
+  }
+
   render() {
+    const { cities, isLoaded } = this.state;
+
+    const cards = !isLoaded ?
+      null :
+      <section className={styles.cardArea}>
+        {cities.map(c => <HomeCard key={c.id} city={c}/>)}
+      </section>
+
     return (
       <Fragment>
         <Navbar />
@@ -13,9 +36,7 @@ class Home extends Component {
           <h1>Cycling Japan</h1>
         </header>
         <main>
-          <section className={styles.cardArea}>
-            <HomeCard />
-          </section>
+          {cards}
           <section className={styles.about}>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             <p>Hello, we’re cockneys old man in pub knavish rogue, rugged driving gloves wario theodore roosevelt knavish rogue hello, we’re cockneys old man in pub Sergeant major. Sterling casual style funny walk tricky sneezes will you do the fandango, funny walk et sodales cum will you do the fandango tom selleck des lynam tricky sneezes sterling quis iron tache dolor ipsum 118 118 casual style kris kristofferson.</p>
--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import styles from './HomeCard.module.css';
 
-const HomeCard = props => {
-  return (
-    <article className={styles.card}>
-      <img src="https://images.unsplash.com/photo-1548652857-5d6ca02aea68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1268&q=80"/>
-      <section className={styles.cardText}>
-        <h3>title</h3>
-        <p>content snippet</p>
-      </section>
-    </article>
-  )
+class HomeCard extends Component {
+  state = {
+    imgURL: '',
+    author: '',
+  }
+
+  componentDidMount() {
+    const { featured_media, author } = this.props.city;
+    const getImageURL = axios.get(`/wp-json/wp/v2/media/${featured_media}`);
+    const getAuthor = axios.get(`/wp-json/wp/v2/users/${author}`);
+
+    Promise.all([getImageURL, getAuthor]).then(res => this.setState({
+      imgURL: res[0].data.media_details.sizes.full.source_url,
+      author: res[1].data.name,
+    }));
+  }
+
+  render() {
+    const { city } = this.props;
+    const { imgURL, author} = this.state;
+
+    return (
+      <article className={styles.card}>
+        <img src={imgURL}/>
+        <section className={styles.cardText}>
+          <h3>{city.title.rendered}</h3>
+          <h4 className={styles.author}>{author}</h4>
+          <p dangerouslySetInnerHTML={{__html: city.excerpt.rendered}}></p>
+        </section>
+      </article>
+    )
+  }
 }
 
 export default HomeCard;
